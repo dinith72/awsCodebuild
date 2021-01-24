f519563555defa114a085945272908de8dbdd40c
dinith72 <jdinith72@gmail.com>
2021-01-24 12:02:15 +0530

    only publish directory is zipped

--- scripts/pipeline/release.sh
+++ scripts/pipeline/release.sh
@@ -10,8 +10,9 @@ rm -rf $local_path
 # get current directory 
 pwd
 # Create a zip of the current directory.
+cd ./TestApplication/bin/Release/netcoreapp3.1/publish/
 #zip -r $local_path . -x .git/ .git/*** .github/workflows/release.yml scripts/pipeline/release.sh scripts/pipeline/upload_file_to_s3.py .DS_Store
-zip -r $local_path ./TestApplication/bin/Release/netcoreapp3.1/publish/
+zip -r $local_path .
 
 # Install required dependencies for Python script.
 pip3 install boto3
